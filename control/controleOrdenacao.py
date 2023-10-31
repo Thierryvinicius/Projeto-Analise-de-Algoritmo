8dfb09b45829d2086a6db52227bdf6d9f4d597dc
Thierry <thierryv3011@gmail.com>
2023-10-31 16:17:05 -0400

    arrumando um erro de implementação

--- control/controleOrdenacao.py
+++ control/controleOrdenacao.py
@@ -10,11 +10,13 @@ def process_sorting(method, input_data):
         return sorted_dict
 
     if method == "Heapsort":
-        sorted_data = Heapsort.Heapsort(input_data)
         heapsort_instance = Heapsort.Heapsort(input_data)
         sorted_dict = heapsort_instance.sort_dictionary()
+        print(sorted_dict)
+        return sorted_dict
 
     if method == "Quicksort":
-        sorted_data = Quicksort.QuickSort(input_data)
         quicksort_instance = Quicksort.QuickSort(input_data)
         sorted_dict = quicksort_instance.sort_dictionary()
+        print(sorted_dict)
+        return sorted_dict
